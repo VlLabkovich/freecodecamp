670ea374da5122bdaed6de522af8c8b037c373fd
Vladislav <labkovich.vl@gmail.com>
2023-10-06 00:38:26 +0300

    init: start comments task

---
+++ JS/Basic JavaScript/comment your javascript code.js
@@ -0,0 +1,11 @@
+// DESCRIPTION:
+// You should create a // style comment that contains at least five letters.
+// You should create a /* */ style comment that contains at least five letters.
+
+// SOLUTION:
+
+// My first task
+
+/*
+My first task
+*/
