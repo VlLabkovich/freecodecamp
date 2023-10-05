b2a75a0fcc35f33254380e639dc92a8ec42c73b6
Vladislav <labkovich.vl@gmail.com>
2023-10-06 00:41:57 +0300

    init: start declare string variables task

---
+++ JS/Basic JavaScript/declare string variables.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Create two new string variables: myFirstName and myLastName and assign them
+// the values of your first and last name, respectively.
+
+// SOLUTION:
+
+var myFirstName = "Vladislav";
+var myLastName = "Labkovich";
