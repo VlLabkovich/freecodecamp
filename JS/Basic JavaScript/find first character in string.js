817b9aef4ed1388aee3480a420b6e692ea2d4c25
Vladislav <labkovich.vl@gmail.com>
2023-10-14 01:22:39 +0300

    init: start task find first character in string

---
+++ JS/Basic JavaScript/find first character in string.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Use bracket notation to find the first character in the lastName variable and assign it to firstLetterOfLastName.
+
+// SOLUTION:
+
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+firstLetterOfLastName = lastName[0];
