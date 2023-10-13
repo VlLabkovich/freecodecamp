73341acd3d0fa667f67e618d154dbc1b4b804ecc
Vladislav <labkovich.vl@gmail.com>
2023-10-14 01:07:08 +0300

    init: start task find string length

---
+++ JS/Basic JavaScript/find string length.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Use the .length property to set lastNameLength to the number of characters in lastName.
+
+// SOLUTION:
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
