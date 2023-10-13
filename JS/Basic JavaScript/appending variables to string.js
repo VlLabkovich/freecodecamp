6f4c75b98c23ec83053dbf1ecde6f516e5812dd2
Vladislav <labkovich.vl@gmail.com>
2023-10-14 00:58:40 +0300

    init: start task appending variables

---
+++ JS/Basic JavaScript/appending variables to string.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+// SOLUTION:
+
+const someAdjective = "cool";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
