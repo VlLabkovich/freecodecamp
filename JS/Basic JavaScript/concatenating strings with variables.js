867db57fcba2912af0d2d856be434057ce6acd8c
Vladislav <labkovich.vl@gmail.com>
2023-10-14 00:52:30 +0300

    init: start task concatenating strings

---
+++ JS/Basic JavaScript/concatenating strings with variables.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Set myName to a string equal to your name and build myStr with myName between the strings My name is  and  and I am well!
+
+// SOLUTION:
+
+const myName = "Vladislav";
+const myStr = "My name is " + myName + " and I am well!";
