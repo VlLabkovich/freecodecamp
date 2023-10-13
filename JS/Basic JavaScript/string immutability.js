ac84cba2ba11329f61e278d081da12dc2765e49c
Vladislav <labkovich.vl@gmail.com>
2023-10-14 01:38:29 +0300

    init: start task change variable

---
+++ JS/Basic JavaScript/string immutability.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Correct the assignment to myStr so it contains the string value of Hello World.
+// let myStr = "Jello World";
+// myStr[0] = "H"; 
+
+// SOLUTION:
+
+let myStr = "Jello World";
+myStr = "Hello World";
