af62fc61b62d25142185b3390cab0480936ceb40
Vladislav <labkovich.vl@gmail.com>
2023-10-15 08:30:32 +0300

    init: start task use bracket notation

---
+++ JS/Basic JavaScript/use bracket notation .js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Let's try to set thirdLetterOfLastName to equal the third letter of the lastName variable using bracket notation.
+
+// SOLUTION:
+
+const lastName = "Lovelace";
+const thirdLetterOfLastName = lastName[2];
