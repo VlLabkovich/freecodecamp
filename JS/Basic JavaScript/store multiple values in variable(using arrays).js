fe9a39dbbb5fea43951980f3f0e95d8ee351ef33
Vladislav <labkovich.vl@gmail.com>
2023-11-01 12:40:34 +0300

    init: start task store values

---
+++ JS/Basic JavaScript/store multiple values in variable(using arrays).js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// Modify the new array myArray so that it contains both a string and a number (in that order).
+
+// SOLUTION:
+
+// Only change code below this line
+const myArray = [];
