31445b1da0ae158f590dd4b4d4c940fb4029f9ea
Vladislav <labkovich.vl@gmail.com>
2023-11-11 01:49:34 +0300

    init: start task create multi-demensional array

---
+++ JS/Basic JavaScript/multi-demensional array.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+// The first element in each sub-array should contain a string with the name of the item.
+// The second element should be a number representing the quantity i.e.
+// There should be at least 5 sub-arrays in the list.
+
+// SOLUTION:
+
+const myList = [];
